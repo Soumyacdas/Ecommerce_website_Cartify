e482f4ff406c798411391be601a7304189b9dc56
Soumya C <soumyac25195@gmail.com>
2023-05-09 12:04:20 +0530

    bug fixed in local

--- Cartify_Shopping/settings.py
+++ Cartify_Shopping/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
+#debug is set to true
 
 ALLOWED_HOSTS = ["*"]
 
